b092e713b1da103aee71bbabb3da9e5f61d9ce16
Faisal-Sey <faisal.issaka@amalitech.com>
2023-04-11 13:29:46 +0000

    updated contact get

--- services/contactServices.js
+++ services/contactServices.js
@@ -5,7 +5,7 @@ import { ContactModel } from '../models/index.js';
 export const createContact = async (data) => ContactModel.create(data);
 export const createMultipleContacts = async (data) => ContactModel.insertMany(data);
 
-export const getContact = async (id, data) => ContactModel.findOne({ id }, data);
+export const getContact = async (id, data) => ContactModel.findOne({ _id: id }, data);
 export const getContacts = async (data) => ContactModel.find({}, data);
 
 export const updateContact = async (phone, otherData) => ContactModel.updateOne({ phone }, otherData);
